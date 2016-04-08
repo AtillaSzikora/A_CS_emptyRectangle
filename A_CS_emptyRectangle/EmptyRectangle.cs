8955e84be8bdd6a52be46059f184c7664badf184
AtillaSzikora <szikora.atilla@gmail.com>
2016-04-08 15:44:40 +0200

    Initial commit

---
+++ A_CS_emptyRectangle/EmptyRectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace A_CS_emptyRectangle {
+
+    internal class EmptyRectangle {
+
+        //Draws an empty rectangle from given character and entered num of rows and cols.
+        private static void Main(string[] args) {
+
+            Console.Write("Enter a character to draw a rectangle: ");
+            var c = char.Parse(Console.ReadLine());
+            Console.Write("Enter how many columns should the rectangle has: ");
+            var col = int.Parse(Console.ReadLine());
+            Console.Write("Enter how many rows should the rectangle has: ");
+            var row = int.Parse(Console.ReadLine());
+
+            var colTimesChar = new string(c, col);
+            var colTimesSpace = new string(' ', col - 2);
+
+            Console.WriteLine(colTimesChar);
+            for (var i = 0; i < row - 2; i++) { Console.WriteLine(c + colTimesSpace + c); }
+            Console.WriteLine(colTimesChar);
+        }
+    }
+}
